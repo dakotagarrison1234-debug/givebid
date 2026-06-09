2a530ccb9fe066936c056d752e9ec9c1fefcd030
Dakota Garrison <dakotagarrison@Mac-4593.lan>
2026-06-08 23:07:56 -0400

    fix clerk imports on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { prisma } from "@/lib/prisma";
-import { auth } from "@clerk/nextjs/server";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default async function HomePage() {
